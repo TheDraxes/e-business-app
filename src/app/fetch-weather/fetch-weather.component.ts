cb772c3e7f01e24b13dcae78313f16878cf9f5be
Tim Vinzing <tim.vinzing@web.de>
2020-03-18 17:29:42 +0100

    some stuff

--- src/app/fetch-weather/fetch-weather.component.ts
+++ src/app/fetch-weather/fetch-weather.component.ts
@@ -9,10 +9,12 @@ import { OwmApiService } from '../owm-api.service';
 export class FetchWeatherComponent implements OnInit {
 
   data: any;
+  iconUrl: string;
 
   @Input() set name(value: string) {
-    this.getWeatherByTownName(value);
-    console.log(this.data);
+    if (value) {
+      this.getWeatherByTownName(value);
+    }
   }
 
   constructor(private owmApiService: OwmApiService) { }
@@ -22,10 +24,23 @@ export class FetchWeatherComponent implements OnInit {
   }
 
   getWeatherByTownName(name: string) {
-    this.owmApiService.getDataByTownName(name).subscribe((data) => {
-      console.log(data);
-      this.data = data;
-    });
+    this.owmApiService.getDataByTownName(name).subscribe(
+      data => { this.data = data; },
+      err  => {},
+      ()   => { this.iconUrl = this.extractIconUrl(); console.log(this.data); }
+    );
+  }
+
+  extractIconUrl() {
+    const iconID = this.data.weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+
+    return iconUrl;
+  }
+
+  convertTimestamp(timeStamp: any) {
+    const today = new Date(timeStamp * 1000);
+    return '' + today.getHours() + ':' + today.getMinutes();
   }
 
 }
